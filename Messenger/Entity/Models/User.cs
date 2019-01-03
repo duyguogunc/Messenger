4a081d51e78f673951c0088f8f8bf7f1832a904d
Sira1 <bukethisim07@gmail.com>
2019-01-03 14:04:10 +0300

    Mesajlaşma için gerekli model sınıfları

---
+++ Messenger/Entity/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Models
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImageAddress { get; set; }
+        public bool Activity { get; set; }
+    }
+}
