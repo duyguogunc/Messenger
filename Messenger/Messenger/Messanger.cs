2be85851d941ff59978272e350625944b36b0526
can boz <cnboz@hotmail.com>
2019-01-03 13:22:11 +0300

    dsadsa

---
+++ Messenger/Messenger/Messanger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger
+{
+    class Messanger
+    {
+        Console.Writeline("Sondan 2. sıra");
+    }
+}
