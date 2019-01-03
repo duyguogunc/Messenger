31830c539100d5f7f39d4cb045b78a19ca460dbf
Sira1 <bukethisim07@gmail.com>
2019-01-03 14:12:23 +0300

    Mesajlaşma için gerekli modeller

--- Messenger/Entity/Models/User.cs
+++ Messenger/Entity/Models/User.cs
@@ -10,6 +10,8 @@ namespace Entity.Models
     {
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
         public string ImageAddress { get; set; }
         public bool Activity { get; set; }
     }
