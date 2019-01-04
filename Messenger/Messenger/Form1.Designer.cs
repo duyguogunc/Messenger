98e0da5e60b01eb0e63431fa71516cf6a44dcf09
Egitmen <duygu.ogunc@wissenakademie.com>
2019-01-04 08:53:01 +0300

    ignore

--- Messenger/Messenger/Form1.Designer.cs
+++ Messenger/Messenger/Form1.Designer.cs
@@ -36,17 +36,19 @@
             // 
             // rch_GelenYazi
             // 
-            this.rch_GelenYazi.Location = new System.Drawing.Point(12, 12);
+            this.rch_GelenYazi.Location = new System.Drawing.Point(9, 10);
+            this.rch_GelenYazi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rch_GelenYazi.Name = "rch_GelenYazi";
-            this.rch_GelenYazi.Size = new System.Drawing.Size(620, 296);
+            this.rch_GelenYazi.Size = new System.Drawing.Size(466, 261);
             this.rch_GelenYazi.TabIndex = 0;
             this.rch_GelenYazi.Text = "";
             // 
             // rch_GidenYazi
             // 
-            this.rch_GidenYazi.Location = new System.Drawing.Point(12, 351);
+            this.rch_GidenYazi.Location = new System.Drawing.Point(9, 285);
+            this.rch_GidenYazi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rch_GidenYazi.Name = "rch_GidenYazi";
-            this.rch_GidenYazi.Size = new System.Drawing.Size(620, 123);
+            this.rch_GidenYazi.Size = new System.Drawing.Size(466, 101);
             this.rch_GidenYazi.TabIndex = 1;
             this.rch_GidenYazi.Text = "";
             // 
@@ -55,9 +57,10 @@
             this.btn_Gonder.BackgroundImage = global::Messenger.Properties.Resources.mail_send;
             this.btn_Gonder.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn_Gonder.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn_Gonder.Location = new System.Drawing.Point(652, 418);
+            this.btn_Gonder.Location = new System.Drawing.Point(489, 340);
+            this.btn_Gonder.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Gonder.Name = "btn_Gonder";
-            this.btn_Gonder.Size = new System.Drawing.Size(108, 56);
+            this.btn_Gonder.Size = new System.Drawing.Size(81, 46);
             this.btn_Gonder.TabIndex = 2;
             this.btn_Gonder.Text = "GÖNDER";
             this.btn_Gonder.UseVisualStyleBackColor = true;
@@ -65,21 +68,22 @@
             // lst_KisiListesi
             // 
             this.lst_KisiListesi.FormattingEnabled = true;
-            this.lst_KisiListesi.ItemHeight = 16;
-            this.lst_KisiListesi.Location = new System.Drawing.Point(652, 12);
+            this.lst_KisiListesi.Location = new System.Drawing.Point(489, 10);
+            this.lst_KisiListesi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lst_KisiListesi.Name = "lst_KisiListesi";
-            this.lst_KisiListesi.Size = new System.Drawing.Size(131, 340);
+            this.lst_KisiListesi.Size = new System.Drawing.Size(99, 277);
             this.lst_KisiListesi.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(795, 496);
+            this.ClientSize = new System.Drawing.Size(596, 403);
             this.Controls.Add(this.lst_KisiListesi);
             this.Controls.Add(this.btn_Gonder);
             this.Controls.Add(this.rch_GidenYazi);
             this.Controls.Add(this.rch_GelenYazi);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
