8600a406cf8644834b7523076df15686ef248e4e
can boz <cnboz@hotmail.com>
2019-01-04 09:05:41 +0300

    aa

---
+++ Messenger/BLL/SqlHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class SqlHelper
+    {
+        public string connetionString { get; set; }
+        public SqlConnection conn { get; set; }
+
+        public SqlHelper()
+        {
+            connetionString =
+                @"Data Source =185.23.75.48,8081\SQLEXPRESS;
+                Initial Catalog=WissenMM;
+                User ID=wissen302;
+                Password=123456";
+            conn=new SqlConnection(connetionString);
+        }
+
+        public void InsertMessage(int? SenderID,int? Receiver,string Message)
+        {
+
+        }
+       
+    }
+}
---
+++ Messenger/BusinessLogicLayer/SqlHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLayer
+{
+    class SqlHelper
+    {
+        public string connectionString { get; set; }
+        public SqlConnection conn { get; set; }
+
+        public SqlHelper()
+        {
+            connectionString = @"Data Source=185.23.75.48,8081\SQLEXPRESS;
+                                    Initial Catalog=WissenMM;
+                                      User ID=wissen302;Password=123456";
+            conn = new SqlConnection(connectionString);
+        }
+
+     
+    }
+}
